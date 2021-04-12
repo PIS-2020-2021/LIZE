15e00fffb5341b785a5afeaff16ba12916e10479
endix24 <enunezma82@alumnes.ub.edu>
2021-04-12 17:44:19 +0200

    Merge Arnau-Enrique

--- app/src/main/java/com/example/lize/workers/MainActivity.java
+++ app/src/main/java/com/example/lize/workers/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.lize.workers;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.view.MenuItem;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.lize.R;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+;
 
 /** Activity Principal de la app Lize. Contenedor del Ámbito con sus Carpetas y sus Notas. */
 public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener{
@@ -17,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuIte
     private FolderHostFragment folderHostFragment;           // Contenedor de Folders
     private FloatingActionButton addFAB;                     // Floating Action Button de añadir
     private boolean cardNoteType = true;                     // Boolean del tipo de vista de las Notas.
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
 
     /** Main constructor */
     @Override
@@ -32,6 +39,16 @@ public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuIte
 
         topAppBar.setOnMenuItemClickListener(this);
         folderHostFragment.registerChipListener(noteHostFragment);
+
+        addFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        new Intent(getApplicationContext(), NotasActivity.class),
+                        REQUEST_CODE_ADD_NOTE
+                );
+            }
+        });
     }
 
     /**
@@ -67,4 +84,13 @@ public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuIte
         item.setIcon((cardNoteType) ? R.drawable.ic_baseline_table_rows_24 :
                 R.drawable.ic_baseline_view_module_24);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK){
+
+        }
+    }
+
 }
