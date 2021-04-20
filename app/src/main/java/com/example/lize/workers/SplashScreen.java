e894ddf042949ae1e15988e52e391c016e78ff82
lisa31419 <isa31419@gmail.com>
2021-04-20 16:58:31 +0200

    Splash screen done

---
+++ app/src/main/java/com/example/lize/workers/SplashScreen.java
@@ -0,0 +1,21 @@
+package com.example.lize.workers;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
+    }
+}
